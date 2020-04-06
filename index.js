02faf212ab3918f9ce2fb3b9c69545385e1b321f
timmson <timmson666@mail.ru>
2020-04-06 12:06:42 +0300

    Fix Security Issues

--- index.js
+++ index.js
@@ -2,51 +2,41 @@ const r = require("request");
 
 let that = null;
 
-
-function device(mac, ip, hostname, alive, last_see_time, active_time, link_type, link_speed) {
-    this.mac = mac;
-    this.ip = ip;
-    this.hostname = hostname;
-    this.alive = alive;
-    this.last_see_time = last_see_time;
-    this.active_time = active_time;
-    this.link_type = link_type;
-    this.link_speed = link_speed;
-}
-
 function SerCommRv6699Api(options) {
-    if (options) {
-        this.url = options.url;
-        this.login = options.login;
-        this.password = options.password;
-    }
-    that = this;
-    that.errCount = 0;
+	if (options) {
+		this.url = options.url;
+		this.login = options.login;
+		this.password = options.password;
+	}
+	that = this;
+	that.errCount = 0;
 }
 
 SerCommRv6699Api.prototype.getDeviceList = () => {
-    return new Promise((resolve, reject) => {
-        r(
-            {
-                uri: that.url,
-                auth: {
-                    user: that.login,
-                    pass: that.password,
-                    sendImmediately: false
-                }
-            }, (err, response, body) => {
-                if (err) {
-                    console.log(err);
-                    reject(err);
-                } else {
-                    let lines = body.split("\n");
-                    eval(lines[202]);
-                    let list = device_array || [];
-                    resolve(list);
-                }
-            }
-        );
-    });
+	return new Promise((resolve, reject) => {
+		r(
+			{
+				uri: that.url,
+				auth: {
+					user: that.login,
+					pass: that.password,
+					sendImmediately: false
+				}
+			}, (err, response, body) => {
+				if (err) {
+					console.log(err);
+					reject(err);
+				} else {
+					let src = body.split("\n")
+						.filter((l, i) => ((i >= 188 && i <= 199) || i === 202))
+						.map((l, i, a) => (i === a.length - 1) ? l.split("=")[1] : l)
+						.join("\n");
+					let deviceArray = eval(src);
+					resolve(deviceArray);
+				}
+			}
+		);
+	});
 };
 
 module.exports = SerCommRv6699Api;
--- test/test.js
+++ test/test.js
@@ -1,30 +1,30 @@
 const expect = require("chai").expect;
-const nock = require('nock');
+const nock = require("nock");
 const fs = require("fs");
 const path = require("path");
 const SerCommRv6699Api = require("../index");
 
 
 const options = {
-    url: "http://your-route-ip",
-    login: "login",
-    password: "password"
+	url: "http://your-route-ip",
+	login: "login",
+	password: "password"
 };
 
 describe("SerCommRv6699Api", () => {
 
-    let api = new SerCommRv6699Api(options);
+	let api = new SerCommRv6699Api(options);
 
-    describe("#getDeviceList", () => {
+	describe("#getDeviceList", () => {
 
-        beforeEach(() => {
-           nock(options.url).get("/").reply(200, fs.readFileSync(path.join(__dirname, "index.html")));
-        });
+		beforeEach(() => {
+			nock(options.url).get("/").reply(200, fs.readFileSync(path.join(__dirname, "index.html")));
+		});
 
-        it("Should return 5 devices", async () => {
-            let devices = await api.getDeviceList();
-            expect(devices).to.have.lengthOf(5);
-        });
-    });
+		it("Should return 5 devices", async () => {
+			let devices = await api.getDeviceList();
+			expect(devices).to.have.lengthOf(5);
+		});
+	});
 
 });
